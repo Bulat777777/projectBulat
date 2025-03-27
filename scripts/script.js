44e40f3bb03de421b691daed4296a8f37a3dd3fc
Bulat777777 <b07542631@gmail.com>
2025-03-27 17:52:58 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 'use strict'
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно')
+    console.log('Скрипт отработал корректно');
+
+    const menuIcon = document.querySelector(".menu__icon");
+
+    menuIcon.addEventListener('mouseenter', () => {
+        console.log('Мышка наведена на изображение, показываем текст');
+ });
+
+
 });
