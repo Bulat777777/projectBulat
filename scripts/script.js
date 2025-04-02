4802e2eee658a1b7ead0495dcdbed8c53cbc87c2
Bulat777777 <b07542631@gmail.com>
2025-04-02 13:38:36 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,19 +1,26 @@
 'use strict'
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно');
+  console.log('Скрипт отработал корректно');
 
-    const menuIcons = document.querySelectorAll(".menu__icon");
+  const menuIcons = document.querySelectorAll(".menu__icon");
 
-    menuIcons.forEach((item, index) => {
-        const menuText = document.querySelectorAll('.menu__description');
-        item.addEventListener('mouseenter', () => {
-            item.style.opacity = 0.5;
-            // И удаляем атрибут hidden и текст становится видимым
-              menuText[index].removeAttribute('hidden');
-            });
-        
- });
+  const menuText = document.querySelectorAll('.menu__popup');
+  menuIcons.forEach((item, index) => {
+    console.log(item, index);
+    item.addEventListener('mouseenter', () => {
+      item.style.opacity = 0.5;
+      // И удаляем атрибут hidden и текст становится видимым
+      menuText[index].removeAttribute('hidden');
+    });
+    item.addEventListener('mouseleave', () => {
+      item.style.opacity = 1;
+      // И добавляем атрибут hidden и текст становится невидимым
+      menuText[index].setAttribute('hidden', true);
+
+    });
+
+  });
 
 
 });
